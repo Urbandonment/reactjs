f98ee114223fa49d2f7de2100735901c8089fdbf
Urbandonment <66505497+Urbandonment@users.noreply.github.com>
2021-11-14 00:38:51 +0700

    Update Components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
-function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: 'e2',
-      title: 'New TV',
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
-  function addExpenseHandler() {}
+function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  function addExpenseHandler(expense) {
+    setExpenses(function prevState() {
+      return [expense, ...expenses];
+    });
+  }
 
   return (
     <div className='App'>
@@ -42,7 +48,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <NewExpense onAddExpense={addExpenseHandler} />
-        <Expenses expenses={expenses} />
+        <Expenses items={expenses} />
       </header>
     </div>
   );
